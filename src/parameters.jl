0240e9507e6da6fc36ba4cd499b86432e478c7e1
Orso Meneghini <orso82@gmail.com>
2023-08-27 21:35:20 -0700

    evaluate functions at top().time.simulation_start

--- src/parameters.jl
+++ src/parameters.jl
@@ -44,6 +44,8 @@ function Base.getproperty(parameters::AbstractParameters, field::Symbol)
             else
                 error("Should not be here")
             end
+        elseif typeof(parameter.value) <: Function
+            return parameter.value(top(parameters).time.simulation_start)
         else
             tp = typeof(parameter).parameters[1]
             return parameter.value::tp
