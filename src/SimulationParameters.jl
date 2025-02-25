3bbedf6cfba6182eb6753abc382b8a56e233640f
Min-Gu Yoo <mgyoo86@gmail.com>
2025-02-25 10:54:06 -0800

    Export `grouping_parameters`

--- src/SimulationParameters.jl
+++ src/SimulationParameters.jl
@@ -42,6 +42,7 @@ export par2hdf, hdf2par
 export show_modified
 export OptParameter, ↔, opt_parameters, parameters_from_opt!, rand, rand!, float_bounds, nominal_values, opt_labels
 export InexistentParametersFieldException, NotsetParameterException, BadParameterException
+export grouping_parameters
 
 const document = Dict()
 document[Symbol(@__MODULE__)] = [name for name in Base.names(@__MODULE__; all=false, imported=false) if name != Symbol(@__MODULE__)]
